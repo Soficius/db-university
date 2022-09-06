94c7671d90043b63e08465ce483ed6b184b5ac48
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-06 16:43:52 +0200

    es. 2 GB done

--- test.sql
+++ test.sql
@@ -46,7 +46,9 @@ FROM students
 GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT office_address as ufficio, COUNT(*) as totale_insegnanti
+FROM teachers
+GROUP BY office_address;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
