9f55994bdab2015dc304a61dce8ac0848ac5a9c1
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-07 18:21:28 +0200

    esercizio finito

--- test.sql
+++ test.sql
@@ -64,27 +64,54 @@ GROUP BY department_id;
 
 -- ------------------------------------------  QUERY CON JOIN
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-SELECT `students`.*
-FROM `students`
-JOIN `degrees`
-ON `degrees`. `id` = `students` . `degree_id`
-WHERE `degrees`. `name` = "Corso di Laurea in Economia";
+SELECT students.*
+FROM students
+JOIN degrees
+ON degrees. id = students . degree_id
+WHERE degrees. name = "Corso di Laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-SELECT `degrees`.*
-FROM `departments`
-JOIN `degrees`
-ON `departments`.`id` = `degrees`. `department_id`
-WHERE `departments` . `name` = "Dipartimento di Neuroscienze";
+SELECT degrees.*
+FROM departments
+JOIN degrees
+ON departments.id = degrees. department_id
+WHERE departments . name = "Dipartimento di Neuroscienze";
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-SELECT `courses`.*
-FROM `courses`
-JOIN `course_teacher`
-ON `courses`.`id`= `course_teacher`.`course_id`
-WHERE `course_teacher`.`teacher_id` = 44;
+SELECT courses.*
+FROM courses
+JOIN course_teacher
+ON courses.id = course_teacher.course_id
+WHERE course_teacher.teacher_id = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT *
+FROM students
+JOIN degrees
+ON degrees.id = students.degree_id
+JOIN departments
+ON departments.id = degrees.department_id
+ORDER BY students.surname, students.name ASC;
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT degrees.name As corso_laurea, courses.name AS materia, teachers.name AS insegnante
+FROM degrees
+JOIN courses
+ON degrees.id = courses.degree_id
+JOIN course_teacher
+ON courses.id = course_teacher.course_id
+JOIN teachers
+ON teachers.id = course_teacher.teacher_id;
+
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
--- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT DISTINCT teachers.*
+FROM teachers
+JOIN course_teacher
+ON teachers.id = course_teacher.teacher_id
+JOIN courses
+ON courses.id = course_teacher.course_id
+JOIN degrees
+ON degrees.id = courses.degree_id
+JOIN departments
+ON departments.id = degrees.department_id
+WHERE departments.name = "Dipartimento di Matematica";
